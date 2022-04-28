3a3e7cd58a6e5d123b24990eac947fa878c5e880
Ryan Joseph <ryankjoseph@gmail.com>
2022-04-28 16:11:48 -0400

    Change nav menu's visible state on click. (custom)

--- src/components/Navbar/DropdownItem/DropdownItem.jsx
+++ src/components/Navbar/DropdownItem/DropdownItem.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './DropdownItem.scss'
 function DropdownItem(props) {
-    // console.log(props);
-    console.log("HELLO")
     if (props.secondaryMenu){
         return (
                 <div className="dropdown-item open-secondary" onClick={()=>props.setDisplayedMenu(props.secondaryMenu)}>
@@ -14,7 +12,6 @@ function DropdownItem(props) {
         )
     }
     else if(props.primaryMenuName){
-        console.log('PRIMARY', props.primaryMenuName)
         return(
                 <div onClick={()=>{props.setDisplayedMenu(props.primaryMenuName)}} className="dropdown-item go-back">
                     <span className="icon-button">{props.leftIcon}</span>
@@ -25,8 +22,8 @@ function DropdownItem(props) {
     }
     else{
         return (
-            <Link to={props.link}>
-                <div className="dropdown-item">
+            <Link to={props.link} >
+                <div className="dropdown-item" onClick={()=>props.setDropdownDisplay('closeAll')}>
                     <span className="icon-button">{props.leftIcon}</span>
                     <span className="dd-description">{props.description}</span>
                     {props.rightIcon && <span className="icon-button icon-right">{props.rightIcon}</span>}
