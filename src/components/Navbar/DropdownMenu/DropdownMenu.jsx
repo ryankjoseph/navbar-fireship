2d625db67ac80d612315771159af4950d0cd5ab4
Ryan Joseph <ryankjoseph@gmail.com>
2022-04-28 16:12:11 -0400

    Add onEnter height checker for smooth animation.

--- src/components/Navbar/DropdownMenu/DropdownMenu.jsx
+++ src/components/Navbar/DropdownMenu/DropdownMenu.jsx
@@ -5,30 +5,31 @@ import { Link } from 'react-router-dom'
 import { CSSTransition } from 'react-transition-group'
 
 function DropdownMenu(props) {
-  console.log('opajsdjbas', props.dropdown)
-  console.log("HELOOOOOOOOOO")
   const primaryMenuName = `${props.description}-primary-menu`;
   const [displayedMenu, setDisplayedMenu] = useState(primaryMenuName);
+  const [menuHeight, setMenuHeight] = useState(null);
+  function calcHeight(el){
+    const height = el.offsetHeight;
+    setMenuHeight(height)
+  }
   return (
-    <div className="dropdown-menu">
-      <CSSTransition in={displayedMenu === primaryMenuName} unmountOnExit timeout={500} classNames='nav-menu-primary'>
+    <div className="dropdown-menu" style={{height: menuHeight}}>
+      <CSSTransition in={displayedMenu === primaryMenuName} unmountOnExit timeout={500} classNames='nav-menu-primary' onEnter={calcHeight}>
 
         <div className={primaryMenuName}>
           {props.dropdown.map(function (x, i) {
             // if there is a secondary menu, the secondary menu is named that, on click the dropdown item will take you there
             if (x.secondary) {
-              const secondaryMenu = x.description.split(' ').join('-') + '-secondary-menu'
-              console.log(secondaryMenu, 'secondary')
+              const secondaryMenu = x.description.split(' ').join('-') + '-secondary-menu';
               return (
                 <DropdownItem setDisplayedMenu={setDisplayedMenu} secondaryMenu={secondaryMenu} link={x.link} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description={x.description} />
               )
             }
             else {
               return (
-                <DropdownItem link={x.link} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description={x.description} />
+                <DropdownItem dropdownDisplay= {props.dropdownDisplay} setDropdownDisplay={props.setDropdownDisplay} link={x.link} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description={x.description} />
               )
             }
-            console.log("LINK", x.link);
           })}
 
         </div>
@@ -36,16 +37,15 @@ function DropdownMenu(props) {
 
       {props.dropdown.map(function (x, i) {
         if (x.secondary) {
-          const secondaryMenu = x.description.split(' ').join('-') + '-secondary-menu'
-          console.log(secondaryMenu, 'secondary2')
+          const secondaryMenu = x.description.split(' ').join('-') + '-secondary-menu';
           return (
             // create CSS transition
-            <CSSTransition in={displayedMenu === secondaryMenu} unmountOnExit timeout={500} classNames='nav-menu-secondary'>
+            <CSSTransition in={displayedMenu === secondaryMenu} unmountOnExit timeout={500} classNames='nav-menu-secondary' onEnter={calcHeight}>
               <div className={secondaryMenu}>
-                <DropdownItem setDisplayedMenu={setDisplayedMenu} primaryMenuName={primaryMenuName} link={x.link} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description={x.description} />
+                <DropdownItem setDisplayedMenu={setDisplayedMenu} primaryMenuName={primaryMenuName} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description="Previous menu" />
                 {
                   x.secondary.map(function (x, i) {
-                    return <DropdownItem link={x.link} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description={x.description} />
+                    return <DropdownItem dropdownDisplay= {props.dropdownDisplay} setDropdownDisplay={props.setDropdownDisplay} link={x.link} leftIcon={x.leftIcon} rightIcon={x.rightIcon} description={x.description} />
                   })
                 }
               </div>
