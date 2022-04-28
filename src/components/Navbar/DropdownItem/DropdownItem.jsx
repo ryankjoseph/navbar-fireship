b85e32c11629dd02c60ef5530d0a6ada76af938b
Ryan Joseph <ryankjoseph@gmail.com>
2022-04-28 02:36:03 -0400

    Make the dropdown item conditional.

--- src/components/Navbar/DropdownItem/DropdownItem.jsx
+++ src/components/Navbar/DropdownItem/DropdownItem.jsx
@@ -4,15 +4,36 @@ import './DropdownItem.scss'
 function DropdownItem(props) {
     // console.log(props);
     console.log("HELLO")
-    return (
-        <Link to={props.link}>
-            <div className="dropdown-item">
-                <span className="icon-button">{props.leftIcon}</span>
-                <span className="dd-description">{props.description}</span>
-                {props.rightIcon && <span className="icon-button icon-right">{props.rightIcon}</span>}
-            </div>
-        </Link>
-    )
+    if (props.secondaryMenu){
+        return (
+                <div className="dropdown-item open-secondary" onClick={()=>props.setDisplayedMenu(props.secondaryMenu)}>
+                    <span className="icon-button">{props.leftIcon}</span>
+                    <span className="dd-description">{props.description}</span>
+                    {props.rightIcon && <span className="icon-button icon-right">{props.rightIcon}</span>}
+                </div>
+        )
+    }
+    else if(props.primaryMenuName){
+        console.log('PRIMARY', props.primaryMenuName)
+        return(
+                <div onClick={()=>{props.setDisplayedMenu(props.primaryMenuName)}} className="dropdown-item go-back">
+                    <span className="icon-button">{props.leftIcon}</span>
+                    <span className="dd-description">{props.description}</span>
+                    {props.rightIcon && <span className="icon-button icon-right">{props.rightIcon}</span>}
+                </div>
+        )
+    }
+    else{
+        return (
+            <Link to={props.link}>
+                <div className="dropdown-item">
+                    <span className="icon-button">{props.leftIcon}</span>
+                    <span className="dd-description">{props.description}</span>
+                    {props.rightIcon && <span className="icon-button icon-right">{props.rightIcon}</span>}
+                </div>
+            </Link>
+        )
+    }
 }
 
 export default DropdownItem
